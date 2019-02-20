0fef673891cff58ed6573745b95f3ede398823a4
nikhiljainlive <nikhiljain5195@gmail.com>
2019-02-20 19:22:57 +0530

    datastructures

---
+++ datastructures/Node.java
@@ -0,0 +1,13 @@
+package datastructures;
+
+public class Node 
+{
+	public String data;
+	public Node link;
+	
+	public Node (String d)
+	{
+		data = d;
+		link = null; 								// link of node will be null by default
+	}
+}
